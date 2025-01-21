d8c6ee9d537d660974f7b3b1fdabfbb2753718d0
Vitor Costa <Vitorxd12.12@gmail.com>
2025-01-21 17:27:38 -0300

    Update HelloWorld.java

--- fProgB2_Java/Aula0/src/HelloWorld.java
+++ fProgB2_Java/Aula0/src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld { //aqui eu estou criando uma classe, tipo uma caixa
+    public static void main(String[] args) {
+        //aqui eu estou cruiando um metodo, o metodo principal main
+        // JAVA DIFERENCIA LETRA MINUSCULA. Se recomanda que eu use letras maiuculas para classes
         //desta forma, eu crio um metodo main, dentro da classe HelloWorld.
         System.out.println("oi mundo");
+        //print = escreva
+        //println = escreval
     }
 }
