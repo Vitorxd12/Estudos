a1d653e85ff9410cc91ba41f19aff33bcb02378d
Vitor Costa <Vitorxd12.12@gmail.com>
2025-01-21 17:08:15 -0300

    First HelloWorld in Java

---
+++ fProgB2_Java/Aula0/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld { //aqui eu estou criando uma classe, tipo uma caixa
+    public static void main(String[] args) { //aqui eu estou cruiando um metodo, o metodo principal main
+        //JAVA DIFERENCIA LETRA MINUSCULA
+       //desta forma, eu crio um metodo main, dentro da classe HelloWorld.
+       System.out.println("oi mundo");
+
+    }
+
+}
